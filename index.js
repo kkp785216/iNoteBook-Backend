ed40fdb87ebdb5252942f38a580efbb2c7c8b563
Krishna Kumar <kkp785216@gmail.com>
2022-05-26 09:57:25 +0530

    cross enabled

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const connectToMongoose = require('./db');
 const express = require('express')
-// const cors = require('cors')
+const cors = require('cors')
 
 // Access to fetch at 'http://localhost:5000/api/notes/fetchallnotes' from origin 'http://localhost:3000' has been blocked by CORS policy
 
@@ -9,7 +9,7 @@ connectToMongoose();
 const app = express()
 const port = 5000
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 // Available Routes
